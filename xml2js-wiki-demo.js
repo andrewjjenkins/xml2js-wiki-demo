e1bd6dc2510a1e59da214212a157a2ad21ef9d48
Andrew Jenkins <andrew@lineratesystems.com>
2013-08-16 20:10:53 -0600

    Document demo script, and refactor.
    The demo script had almost no documentation, so add it.
    The demo script's layout was "as coded" but some slight refactoring
    makes it easier to read.

--- xml2js-wiki-demo.js
+++ xml2js-wiki-demo.js
@@ -7,58 +7,8 @@ var xml2js = require('xml2js')
   , fpm = require('lrs/forwardProxyModule');
 
 
-var redirTarget = 'http://en.wikipedia.org/wiki/%(page)s?oldid=%(version)s';
-var redirBody =
-  '<html><head><title>Redirecting %(page)s</title></head>\n' +
-  '<body>\n' +
-  '  <h1>Redirecting to a human-edited version</h1>\n' +
-  '  <p>The last version of %(page)s was edited by an anonymous user.</p>\n' +
-  '  <p>Redirecting to the last human-edited version: \n' +
-  '     <a href="%(target)s">%(target)s</a></p>\n'
-  '</body></html>\n';
-
-function debugInfo(req, resp, next, url, callback) {
-  console.log('url:', url);
-  callback();
-}
-
-function printHelp(req, resp, next, url, callback) {
-  resp.end(sprintf(landingPageBody, {}));
-  callback('Rendered help');
-}
-
-function skipIfNotArticle(req, resp, next, url, callback) {
-  //FIXME: regex to internationalize
-  if (req.headers.host != 'en.wikipedia.org') {
-    callback([ '', req.headers.host + ' is not wikipedia']);
-    return;
-  }
-  if (url.path.substring(0, 6) != '/wiki/') {
-    callback([ '', url.path + ' is not a wiki article']);
-    return;
-  }
-  console.log('Passing url ' + url.path);
-  callback();
-}
-
-function eatErrorBody(resp, callback) {
-  var errorBody = '';
-  resp.on('data', function (chunk) { errorBody += chunk; });
-  resp.on('end', function() { callback(null, errorBody) });
-}
-
-function redirectToVersion(resp, page, version) {
-  var subs = { 'page': page, 'version': version };
-  subs.target = sprintf(redirTarget, subs);
-  var body = sprintf(redirBody, subs);
-  console.log('Redirecting to: ', subs);
-  resp.writeHead(302, 'Found',
-                 { 'Location': subs.target,
-                   'Content-Type': 'text/html',
-                   'Content-Length': body.length });
-  resp.end(body);
-}
-
+// This is the primary decision function.  Returns true if this revision
+// was by a registered user.
 function revisionByRegisteredUser(revision) {
   var contributor = revision.contributor[0];
   if (contributor.username === undefined) {
@@ -81,21 +31,73 @@ function revisionByRegisteredUser(revision) {
   }
 }
 
-function checkArticleRevisions(req, resp, next, url, callback) {
-  var page = url.path.substring(6);
-  if (/\/|:|\?|&/.exec(page)) {
+// PIPELINE STAGE 1
+// If the target of the request is not a plain request for an article,
+// then skip out of the pipeline and don't redirect.
+function skipIfNotArticle(req, resp, next, url, callback) {
+  //FIXME: regex to internationalize
+  if (req.headers.host != 'en.wikipedia.org') {
+    callback([ '', req.headers.host + ' is not wikipedia']);
+    return;
+  }
+  if (url.path.substring(0, 6) != '/wiki/') {
+    callback([ '', url.path + ' is not a wiki article']);
+    return;
+  }
+  if (/\/|:|\?|&/.exec(url.path.substring(6))) {
     callback([ '', 'Page ' + url.path + ' had surprising characters']);
     return;
   }
+  callback();
+}
+
+// Helper to write out the redirection
+var redirTarget = 'http://en.wikipedia.org/wiki/%(page)s?oldid=%(version)s';
+var redirBody =
+  '<html><head><title>Redirecting %(page)s</title></head>\n' +
+  '<body>\n' +
+  '  <h1>Redirecting to a human-edited version</h1>\n' +
+  '  <p>The last version of %(page)s was edited by an anonymous user.</p>\n' +
+  '  <p>Redirecting to the last human-edited version: \n' +
+  '     <a href="%(target)s">%(target)s</a></p>\n'
+  '</body></html>\n';
+function redirectToVersion(resp, page, version) {
+  var subs = { 'page': page, 'version': version };
+  subs.target = sprintf(redirTarget, subs);
+  var body = sprintf(redirBody, subs);
+  console.log('Redirecting to: ', subs);
+  resp.writeHead(302, 'Found',
+                 { 'Location': subs.target,
+                   'Content-Type': 'text/html',
+                   'Content-Length': body.length });
+  resp.end(body);
+}
+
+
+// PIPELINE STAGE 2
+// Waterfall to do the redirection if necessary:
+//  - Request the revision history of the article from Wikipedia
+//  - Check if the response is an error
+//  - Parse the response body as an XML document
+//  - Walk the list of revisions from most recent to oldest until 
+//     an edit by a registered user is found
+//      - If that's the latest rev, then skip redirect.
+//      - If that's not the latest rev, write a redirect.
+//
+// On any error, just skip redirection and write to the syslog so that
+// the user's request is fulfilled by Wikipedia.
+function checkArticleRevisions(req, resp, next, url, callback) {
+  var page = url.path.substring(6);
   var revisionPath = '/w/index.php?title=Special:Export&pages=' + page
     + '&offset=0&limit=5&action=submit&dir=desc';
 
   async.waterfall([
     function makeRevisionRequest(callback) {
-      var revisionReq = http.request({ host: req.connection.address().address,
-                                       path: revisionPath,
-                                       method: 'POST' /* req'd for good response */},
-                                     function (revResp) { callback(null, revResp); });
+      var revisionReq = http.request(
+        { host: req.connection.address().address,
+          path: revisionPath,
+          method: 'POST' /* req'd for response */},
+        function (revResp) { callback(null, revResp); });
       revisionReq.removeHeader('Host');
       revisionReq.addHeader('Host', 'en.wikipedia.org');
       revisionReq.addHeader('Content-Length', '0');
@@ -113,7 +115,6 @@ function checkArticleRevisions(req, resp, next, url, callback) {
                    util.inspect(revResp.headers) + '\n, body:\n' + body);
         });
       }
-
       var respBody = '';
       revResp.on('data', function(chunk) { respBody += chunk; });
       revResp.on('end', function() {
@@ -124,26 +125,26 @@ function checkArticleRevisions(req, resp, next, url, callback) {
       });
     },
     function parseRevisionResponse(body, callback) {
-      xml2js.parseString(body, function (err, obj) {
-        console.log('Response: ' + util.inspect(obj, false, 3));
-        var revs = obj.mediawiki.page[0].revision;
-//        console.log('Revisions object: ' + util.inspect(revs, false, 3));
-//        console.log('Revs length:', revs.length);
-        for (var i = 0; i < revs.length; ++i) {
-          if (revisionByRegisteredUser(revs[i])) {
-            if (i === 0) {
-              console.log('Yay no redirect');
-              // Yay, no need to redirect.
-              callback();
-            } else {
-              // First rev wasn't human, but we found one.  Redir.
-              redirectToVersion(resp, page, revs[i].id[0]);
-              callback();
-            }
-            return;
-          }
-        }
+      xml2js.parseString(body, function (err, xmlObj) {
+        callback(null, xmlObj);
       });
+    },
+    function redirectToLatestVersionIfNecessary(xmlObj, callback) {
+      var revs = xmlObj.mediawiki.page[0].revision;
+      for (var i = 0; i < revs.length; ++i) {
+        if (revisionByRegisteredUser(revs[i])) {
+          if (i === 0) {
+            console.log('Yay no redirect');
+            // Yay, no need to redirect.
+            callback();
+          } else {
+            // First rev wasn't human, but we found one.  Redir.
+            redirectToVersion(resp, page, revs[i].id[0]);
+            callback();
+          }
+          return;
+        }
+      }
     }
     ],
     function(err, result) {
@@ -156,6 +157,7 @@ function checkArticleRevisions(req, resp, next, url, callback) {
   );
 }
 
+// Glue to intercept requests that arrive at the forward proxy
 fpm.on('exist', 'fp1', function (fp) {
   fp.on('request', function (req, resp, next) {
     var url = urlParser.parse(req.url);
@@ -163,8 +165,6 @@ fpm.on('exist', 'fp1', function (fp) {
       return function(callback) { fn(req, resp, next, url, callback); };
     }
     async.series([ 
-//                   curry(debugInfo),
-//                   curry(printHelp),
                    curry(skipIfNotArticle),
                    curry(checkArticleRevisions),
                  ],
@@ -179,3 +179,15 @@ fpm.on('exist', 'fp1', function (fp) {
   });
   console.log('Listening for requests on fp1');
 });
+
+
+// Helper to just read out the whole body of the response if we get an
+// error reading from wikipedia.
+function eatErrorBody(resp, callback) {
+  var errorBody = '';
+  resp.on('data', function (chunk) { errorBody += chunk; });
+  resp.on('end', function() { callback(null, errorBody) });
+}
+
+
+
